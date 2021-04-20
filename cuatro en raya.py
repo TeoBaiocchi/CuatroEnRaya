e92f895cc0c38d8bed51f333b4adfe48cefb3028
TeoBaiocchi <82824948+TeoBaiocchi@users.noreply.github.com>
2021-04-20 10:34:13 -0300

    Update cuatro en raya.py

--- cuatro en raya.py
+++ cuatro en raya.py
@@ -3,7 +3,7 @@
 
 def tableroVacio(): #esto es una lista con 6 listas de 7 elementos, que conforman el tablero
     return[ 
-        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0], #1ra
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
@@ -11,22 +11,33 @@ def tableroVacio(): #esto es una lista con 6 listas de 7 elementos, que conforma
         [0, 0, 0, 0, 0, 0, 0], #esta es la 6ta
     ]
 
-def completarTableroEnOrden(secuencia, tableroVacio):
+def contenidoColumna(nro_columna, tablero):
+    columna = []
+    for fila in tablero:
+        celda = fila[nro_columna - 1]
+        columna.append(celda)
+    return columna
+
+def completarTableroEnOrden(secuencia, tablero):
     ficha = 1 #(turno)
     for columna in secuencia:
-        tableroVacio = soltarFichaEnTablero(ficha, columna, tableroVacio)
+        soltarFichaEnTablero(ficha, columna, tablero)
         if ficha == 1:
-           ficha = 2
+            ficha = 2
         else:
             ficha = 1
-    return tableroVacio
+    return tablero
+
+def dibujarTablero(tablero): #esta funcion solamente va a imprimir el resultado, por lo tanto tengo
+    for x in tablero: # que pasarle solamente el resultado xD
+        print(x)
     
 def soltarFichaEnTablero(ficha, columna, tablero): #esta funcion la va a llamar completar tablero en orden
     for fila in range(6, 0, -1): #fila esta siendo declarado, range va de 6 a 0 bajando de a 1
         if tablero[fila - 1][columna - 1] == 0:
             tablero[fila - 1][columna - 1] = ficha
             return tablero #el return tiene que estar incluido en el if para que tire una sola ficha por llamado a la funcion
-            #porque de no ser así, se embatata* y revisa 6 veces donde no tiene que hacerlo
+            #porque de no ser así, pasan cosas y revisa 6 veces donde no tiene que hacerlo
 
 def deteccionError(secuencia):
     for x in secuencia:
@@ -35,11 +46,15 @@ def deteccionError(secuencia):
             return 2
     return 1
 
-secuencia = [1, 2, 3, 1, 8]
+secuencia = [1, 2, 3, 1]
+tablero = []
 error = deteccionError(secuencia)
 if error == 1:
-    dibujarTablero( 
-        completarTableroEnOrden(
-            secuencia, tableroVacio() #para completar el tablero le envío el orden de los tiros y la lista de listas
-        )
-    )
+    tablero = completarTableroEnOrden(secuencia, tableroVacio())
+    dibujarTablero(tablero)
+    print()
+    print(contenidoColumna(1, tablero))
+#Esta ultima funcion primero define una secuencia
+#detecta si es correcta, y despues dibuja el tablero
+#para esto lo declara como variable vacia, la llena igualando a completarTableroEnOrden
+#y finalmente lo imprime con otra funcion
